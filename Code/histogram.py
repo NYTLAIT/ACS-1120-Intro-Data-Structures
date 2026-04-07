85b9a8b5025e88d6f44ddb3e80ef539a490f680e
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2026-04-07 13:21:29 -0700

    Lesson 3, Finished building histogram and unique words

--- Code/histogram.py
+++ Code/histogram.py
@@ -3,20 +3,27 @@ import re
 def histogram(source_text):
     # Clean up words
     with open(source_text, 'r') as infile:
-        text = infile.read().lower()
-    words = re.split(r'\W+', text)
-    print(words)
+        raw_text = infile.read()
+
+    split_words = re.split(r"[^\w+']", raw_text)
+    print('Split up words ------', split_words, '\n')
+
+    words = [word.lower() for word in split_words if word]
+    print('Cleaned up words ------', words, '\n')
 
     # Build Historgram
     histogram = {}
     for word in words:
-        if word and word != 1:
-            histogram[word] += 1
-        elif word:
-            histogram[word] = 1
+        histogram[word] = histogram.get(word, 0) + 1
 
     return histogram
 
-print(histogram('shackleton_quotes.txt'))
 
-    
+def unique_words(histogram):
+    return len(histogram)
+
+if __name__ == '__main__':
+    text = 'shackleton_quotes.txt'
+    text_histogram = histogram(text)
+    print('histogram ------', text_histogram)
+    print('unique_words -------', unique_words(text_histogram))
