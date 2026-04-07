a188d356debc7b660aedffae594e958d63538d03
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2026-04-07 12:52:25 -0700

    Lesson 3, cleaned up words

--- Code/histogram.py
+++ Code/histogram.py
@@ -3,16 +3,20 @@ import re
 def histogram(source_text):
     # Clean up words
     with open(source_text, 'r') as infile:
-        raw_text = infile.read().lower()
-    words = re.split(r'\W+', raw_text)
+        text = infile.read().lower()
+    words = re.split(r'\W+', text)
     print(words)
 
     # Build Historgram
     histogram = {}
     for word in words:
-        word.strip()
+        if word and word != 1:
+            histogram[word] += 1
+        elif word:
+            histogram[word] = 1
 
     return histogram
 
-histogram('shackleton_quotes.txt')
+print(histogram('shackleton_quotes.txt'))
+
     
