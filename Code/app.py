d319a6300a9a9b82c3ddd53733f8bfc61d151b1d
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2026-04-09 13:50:43 -0700

    Installed Flask

--- Code/app.py
+++ Code/app.py
@@ -1,17 +1,18 @@
 """Main script, uses other modules to generate sentences."""
 from flask import Flask
 
-
 app = Flask(__name__)
 
 # TODO: Initialize your histogram, hash table, or markov chain here.
 # Any code placed here will run only once, when the server starts.
-
+from histogram import histogram
+from stochastic_sampling import weighted_random_word
 
 @app.route("/")
 def home():
     """Route that returns a web page containing the generated text."""
-    return "<p>TODO: Return a word here!</p>"
+    word = weighted_random_word(histogram('one fish, two fish, red fish, blue fish'))
+    return f"<p>{word}</p>"
 
 
 if __name__ == "__main__":
