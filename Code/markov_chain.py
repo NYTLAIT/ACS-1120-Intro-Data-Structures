06e25ec47b1eeccc7cb94a785b13d57593de86a3
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2026-04-16 13:42:33 -0700

    Lesson 7: Made the random walk

--- Code/markov_chain.py
+++ Code/markov_chain.py
@@ -1,4 +1,6 @@
 from histogram import make_wordslist
+from stochastic_sampling import weighted_random_word
+import random
 
 def make_markov(list_words):
     chain = {}
@@ -14,10 +16,28 @@ def make_markov(list_words):
         chain[current_word][next_word] += 1
     return chain
 
+def random_walk(markov_chain, start_word, num_words):
+    text = [start_word]
+    current_word = start_word.lower().strip()
+
+    for _ in range(num_words - 1):
+        if current_word not in markov_chain:
+            break
+        next_words = markov_chain[current_word]
+        next_word = weighted_random_word(next_words)
+
+        text.append(next_word)
+        current_word = next_word
+    
+    return ' '.join(text) + '.'
+    
 if __name__ == '__main__':
     sample_text = 'I like dogs and you like dogs. I like cats but you hate cats.'
     list_words = make_wordslist(sample_text)
-    print(make_markov(list_words))
+    chain = make_markov(list_words)
+    print(chain)
+    walk = random_walk(chain, 'I', 16)
+    print(walk)
 
 
 
